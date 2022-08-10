c8f78952f7104bdfb6b4ff87d756001db7a517d2
BASSEM <bassem.jadoui@esprit.tn>
2022-08-10 10:26:42 +0100

    Initial commit ✅

---
+++ src/test/java/com/fardi/feignclient/FeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fardi.feignclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
